a14f1abd661690a85242479a23213724af0b53c5
Kelvin Hongquiao Li <kelvin_hq_li@apple.com>
2019-08-29 14:39:34 -0700

    Update chrome

--- chrome/js/content-script.js
+++ chrome/js/content-script.js
@@ -19,4 +19,27 @@ function checkSVGShowOrHide() {
 	}
 }
 
+function moveToOneX() {
+	var list = document.getElementsByTagName('figure');
+	for (var i = list.length - 1; i >= 0; i--) {
+		var style = list[i].currentStyle || window.getComputedStyle(list[i], false);
+		var bgImage = style.backgroundImage.replace("_2x","");
+		if (bgImage.length > 10) {
+			list[i].style.backgroundImage = bgImage;
+		}
+	}
+}
+
+function moveToTwoX() {
+	var list = document.getElementsByTagName('figure');
+	for (var i = list.length - 1; i >= 0; i--) {
+		var style = list[i].currentStyle || window.getComputedStyle(list[i], false);
+		var bgImage = style.backgroundImage.replace("_2x","");
+		if (bgImage.length > 10) {
+			bgImage = bgImage.replace(/(\.jpg|\.png)/g,"_2x$1");
+			list[i].style.backgroundImage = bgImage;
+		}
+	}
+}
+
 // test()
--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -5,22 +5,42 @@ var text2 = "Hide SVG";
 function click(e) {
   chrome.tabs.executeScript(null,
       {code:"runSVGShowOrHide();"});
-  window.close();
+ 	CheckSVGType();
+  // window.close();
+}
+
+function oneX(e) {
+  chrome.tabs.executeScript(null,
+      {code:"moveToOneX();"});
+  // window.close();
+}
+function twoX(e) {
+  chrome.tabs.executeScript(null,
+      {code:"moveToTwoX();"});
+  // window.close();
+}
+
+function CheckSVGType() {
+	var svg = document.getElementById('svg');
+	chrome.tabs.executeScript({code:"checkSVGShowOrHide();"},(results) => {
+  	if (results == 1) {
+  		svg.value = text1;
+  	}else{
+  		svg.value = text2;
+  	}
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-	var divs = document.querySelectorAll('input');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
-	chrome.tabs.executeScript({code:"checkSVGShowOrHide();"},(results) => {
-  	if (results == 1) {
-  		divs[0].value = text1;
-  	}else{
-  		divs[0].value = text2;
-  	}
-  });
+	var svg = document.getElementById('svg');
+	svg.addEventListener('click', click);
 	
-  
+  var x1 = document.getElementById('1x');
+  x1.addEventListener('click',oneX);
+
+  var x2 = document.getElementById('2x');
+  x2.addEventListener('click',twoX);
+
+  CheckSVGType();
 
 });
